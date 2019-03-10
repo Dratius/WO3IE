f2d1d7302467cf11427725d78f2c7c2963b79c04
Drose <drosenenom@gmail.com>
2019-03-10 17:31:24 +0300

    Move Batch Vault To Folder & Reset Ransomwares to new

--- WindowsApp4/Form/User Form/Ransomwares.vb
+++ WindowsApp4/Form/User Form/Ransomwares.vb
@@ -1,9 +1,9 @@
 ﻿Public Class Ransomwares
-    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+    Private Sub Label4_Click(sender As Object, e As EventArgs) 
 
     End Sub
 
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+    Private Sub Label2_Click(sender As Object, e As EventArgs) 
 
     End Sub
 End Class
