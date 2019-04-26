b134ecbc1897b6e888ca33635485a0ef0e792a76
Drose <drosenenom@gmail.com>
2019-04-26 22:16:16 +0300

    modify windows (2) & Form add

--- WindowsApp4/Form/User Form Windows/Tools.Designer.vb
+++ WindowsApp4/Form/User Form Windows/Tools.Designer.vb
@@ -22,8 +22,39 @@ Partial Class Tools
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(222, 518)
+        Me.Panel1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(436, 218)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Tools
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Tools"
+        Me.Size = New System.Drawing.Size(771, 518)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
 End Class
---
+++ WindowsApp4/Form/User Form Windows/Windows Tools.vb
@@ -0,0 +1,3 @@
+﻿Public Class Windows_Tools
+
+End Class
